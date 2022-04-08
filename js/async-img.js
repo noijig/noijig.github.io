9f7a9e396b44cb6a05245c96e20a3a821ab2e2b9
ddevmonkey <99560109+noijig@users.noreply.github.com>
2022-04-08 13:08:38 +0200

    Variable Exposing Commit
    This commit includes all necessary files in order to view the project "Variable Exposing".
    There is also a ghost project included in the .html files that is not included in this commit.

---
+++ js/async-img.js
@@ -0,0 +1,35 @@
+(() => {
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    console.log("Checking for async images");
+    let $images = document.querySelectorAll("[data-img-src]");
+    $images.forEach(($image) => {
+        console.log("Loaded async image", $image);
+        $image.removeAttribute("hidden");
+        /*/
+        sleep(5000).then(() => {
+            image.src = image.getAttribute("data-img-src");
+            image.removeAttribute("data-img-src");
+        })
+        /*/
+        $image.src = $image.getAttribute("data-img-src");
+        $image.removeAttribute("data-img-src");
+        //*/
+    });
+
+    let $isScript = document.querySelectorAll(".is-script");
+    $isScript.forEach(($script => {
+        $script.removeAttribute("hidden");
+        $script.classList.remove("is-hidden");
+    }))
+
+    let $dropdowns = document.querySelectorAll(".people-dropdown");
+    $dropdowns.forEach(($dropdown) => {
+        $dropdown.addEventListener("click", () => {
+            $dropdown.parentElement.classList.toggle("is-active");
+        });
+    });
+})();
